1e738367a21fc03fd56fd6b717e342d162cceca6
Adityax2808 <ii3837@srmist.edu.in>
2025-02-20 15:20:16 +0530

    [Aditya] Add: UC1 to check attendance of employee

--- employee_comp.py
+++ employee_comp.py
@@ -1,1 +1,15 @@
-print("Welcome to Employee Wage Computation Program")
+import random
+
+def check_attendance():
+    attendance = random.randint(0 , 1 , 2)
+    if attendance == 1:
+        print("Employee is Present Full time")
+    elif attendance == 2:
+        print("Employee is Present Part time")
+    else:
+        print("Employeee is Absent")
+
+
+if __name__ == "__main__":
+    check_attendance()
+    print("Welcome to Employee Wage Computation Program")
