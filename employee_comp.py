de400857efae66457a5e3f028504ab908721211f
Adityax2808 <ii3837@srmist.edu.in>
2025-02-20 16:09:23 +0530

    [Aditya] Update: Used switch statement to calculate wage and check attendance

--- employee_comp.py
+++ employee_comp.py
@@ -1,33 +1,27 @@
 import random
 
+def calculate_wage(attendance : int):
+    FULL_TIME_HOUR = 8
+    PART_TIME_HOUR = 4
+    WAGE_PER_HOUR = 20
+
+    switch = {
+        0: 0,                                   #absent
+        1: FULL_TIME_HOUR * WAGE_PER_HOUR,      #full time
+        2: PART_TIME_HOUR * WAGE_PER_HOUR       #part time
+    }
+
+    return switch.get(attendance)
+
 def check_attendance():
     attendance = random.randint(0 , 2)
-    if attendance == 1:
-        print("Employee is Present (Full-time)")
-        calculate_wage(attendance)
-
-    elif attendance == 2:
-        print("Employee is Present (Part-time)")
-        calculate_wage(attendance)
-    else:
-        print("Employeee is Absent")
-        calculate_wage(attendance)
-
-def calculate_wage(attendance : int):
-    WAGE_PER_HOUR = 20
-    ASSUME_FULL_TIME = 8
-    ASSUME_PART_TIME = 4
-
-    if attendance == 1:
-        daily_wage = ASSUME_FULL_TIME * WAGE_PER_HOUR
-
-    elif attendance == 0:
-        daily_wage = 0
-    
-    else:
-        daily_wage = ASSUME_PART_TIME * WAGE_PER_HOUR    
-
-    print(f"Daily Wage: ${daily_wage}")       
+    switch = {
+        0: "Employee is Absent",
+        1: "Employee is Present (Full-Time)",
+        2: "Employee is Present (Part-Time)"
+    }
+    print(switch.get(attendance))
+    print(f"Daily Wages: ${calculate_wage(attendance)}")
 
 if __name__ == "__main__":
     check_attendance()
